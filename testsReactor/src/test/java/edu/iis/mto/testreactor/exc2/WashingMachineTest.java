e0118ab6a0ef69940174707c0d34f707b8470a13
Piotr Popiolek <209437@edu.p.lodz.pl>
2019-05-15 18:11:09 +0200

    refractor Percentage add test Washing Machine Constructor Where All Arguments Are Null

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
@@ -1,15 +1,44 @@
 package edu.iis.mto.testreactor.exc2;
 
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WashingMachineTest {
+
+    DirtDetector dirtDetector;
+    Engine engine;
+    WaterPump waterPump;
+    LaundryBatch laundryBatch;
+    ProgramConfiguration programConfiguration;
+    WashingMachine washingMachine;
+    LaundryStatus status;
+
+    @Before
+    public void init() {
+        dirtDetector = mock(DirtDetector.class);
+        engine = mock(Engine.class);
+        waterPump = mock(WaterPump.class);
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
     }
+
+    @Test(expected = NullPointerException.class)
+    public void WashingMachineTestConstructorWhereAllArgumentsAreNull() {
+        new WashingMachine(null, null, null);
+    }
+
+
+
+
+
 
 }
